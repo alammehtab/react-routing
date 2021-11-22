b3ab3203c6b06e77e30db2bd2501822d5b2bc913
Mehtab Alam <49854015+alammehtab@users.noreply.github.com>
2021-11-22 12:39:31 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import About from './pages/About'
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
